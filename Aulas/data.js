ab11959c53dafbbba40c1aadde14fcfb3828b4ac
Bernardo <beefreguglia@gmail.com>
2020-07-01 12:29:19 -0300

    Feito as aulas para tirar o texto do frontend e colocar no backend

---
+++ Aulas/data.js
@@ -0,0 +1,60 @@
+module.exports = [
+    
+    {
+
+        id: "rCeGfFk-uCk",
+        title: "Setup para apps Node.js (TypeScript, ESLint, Jest, Babel) | Code/Drops #33",
+        duration: "45:38",
+        price: "FREE"
+
+    },
+
+    {
+
+        id: "hN0zWNuZJC4",
+        title: "Deno, Dashboard, Frustração na Carreira, Eject do Expo, Monorepo | #PR 08",
+        duration: "11:06",
+        price: "FREE"
+
+    },
+
+    {
+
+        id: "xDFzOp2BoS8",
+        title: "Open Source? Como virar Pleno? TypeORM? Emprego antes dos 18? Context API vs. Redux? | #PR 07",
+        duration: "09:06",
+        price: "FREE"
+
+    },
+
+    {
+
+        id: "NLcXijzqP9M",
+        title: "Começar pelo TypeScript ou JavaScript? Python, Deno, React Native ou Flutter? | #PR 05",
+        duration: "07:18",
+        price: "FREE"
+
+
+    },
+
+    {
+
+        id: "XomJBk4krnI",
+        title: "Idade na programação, Medo de entrevista, Realocação no Mercado e Trabalho Remoto | #PR 02",
+        duration: "17:20",
+        price: "FREE"
+
+
+    },
+
+    {
+
+        id: "f7jMO95AHmA",
+        title: "Qual teclado o Diego usa? Adonis V5 + Typescript? Sequelize ou TypeORM? O que é Hermes? | #PR 03",
+        duration: "15:14",
+        price: "FREE"
+
+
+    }
+
+]
--- Aulas/server.js
+++ Aulas/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const videos = require("./data")
 
 const server = express()
+
 
 server.use(express.static('public'))
 
@@ -9,19 +11,51 @@ server.set("view engine", "njk")
 
 nunjucks.configure("views", {
 
-    express: server
+    express: server,
+    //Serve para 
+    autoescape: false
 
 })
 
 server.get("/", function(req, res){
 
-    return res.render("about")
+    const about = {
+
+        avatar_url:"https://avatars3.githubusercontent.com/u/64103507?s=400&u=7fb2f781cf0eff6ef2e91758c08fecd83b48d5b1&v=4",
+        name:"Bernardo Freguglia",
+        role:"Aluno Rocketseat",
+        descripition:'Programador FullStack Júnior, aluno da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a> ',
+        links: [
+
+            {
+                name: "Github",
+                url: "https://github.com/beefreguglia"
+
+            },
+
+            {
+                name: "Twitter",
+                url: "https://twitter.com/beefreguglia"
+
+            },
+
+            {
+                name: "LinkedIn",
+                url: "https://www.linkedin.com/in/bernardo-freguglia/"
+
+            }
+
+        ]
+
+    }
+
+    return res.render("about", {about})
 
 })
 
 server.get("/portfolio", function(req, res){
 
-    return res.render("portfolio")
+    return res.render("portfolio", {items: videos})
 
 })
 
